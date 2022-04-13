b0f79d822fa002631a37265ec4c2e113d85e86bb
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2022-04-12 21:52:30 -0500

    setup items

--- src/main/java/com/jetpacker06/CreateBrokenBad/AllItems.java
+++ src/main/java/com/jetpacker06/CreateBrokenBad/AllItems.java
@@ -1,16 +1,24 @@
 package com.jetpacker06.CreateBrokenBad;
 
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class AllItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, CreateBrokenBad.MOD_ID);
     //begin items
-
+    public static final RegistryObject<Item> ephedra_juice = ingredient("ephedra_juice");
     //end items
+    public static final RegistryObject<Item> ingredient(String name) {
+        return ITEMS.register(name, () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    }
+    public static final RegistryObject<Item> ingredient(String name, Item.Properties pProperties) {
+        return ITEMS.register(name, () -> new Item(pProperties));
+    }
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
