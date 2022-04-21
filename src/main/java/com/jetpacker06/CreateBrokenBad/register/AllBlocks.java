2fd9a9398e9f1a7cec7a3fbe7e6ab9d306a42457
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2022-04-21 18:07:06 -0500

    tweak

--- src/main/java/com/jetpacker06/CreateBrokenBad/register/AllBlocks.java
+++ src/main/java/com/jetpacker06/CreateBrokenBad/register/AllBlocks.java
@@ -22,12 +22,13 @@ import java.util.function.Supplier;
 public class AllBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, CreateBrokenBad.MOD_ID);
     public static final Item.Properties defaultProperties = new Item.Properties().tab(ItemGroup.CREATEBB);
-
+    public static void register(IEventBus eventBus) {BLOCKS.register(eventBus);}
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn, tab);
         return toReturn;
     }
+
     private static <T extends Block> RegistryObject<T> registerCallBellBlock(String name, Supplier<T> block, Item.Properties pProperties) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerCallBellBlockItem(name, toReturn, pProperties);
@@ -39,5 +40,5 @@ public class AllBlocks {
 
     private static <T extends Block>RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab) {return AllItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));}
     private static <T extends Block>RegistryObject<Item> registerCallBellBlockItem(String name, RegistryObject<T> block, Item.Properties pProperties) {return AllItems.ITEMS.register(name, () -> new BrassCallBellBlockItem(block.get(), pProperties));}
-    public static void register(IEventBus eventBus) {BLOCKS.register(eventBus);}
+
 }
